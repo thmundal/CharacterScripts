9bd506df3d536306f9ff649c457b260112726e3c
Thomas Mundal <thmundal@gmail.com>
2013-03-01 15:56:25 +0100

    Cleaned up unused code.

--- Assets/Character Scripts/EnemyAI.cs
+++ Assets/Character Scripts/EnemyAI.cs
@@ -81,58 +81,18 @@ public class EnemyAI : MonoBehaviour {
 					if(Vector3.Distance(_myTransform.position, hit.transform.position) < Vector3.Distance(_myTransform.position, target.position)) {
 						// If this is true, it indicates that something is between us and the player
 					
-						//_stopMoving = true;
-						//Debug.Log ("There is something between us and the player, we are stopping");
-						
-						// Calculate Path to take
+						// Calculate Path to take around the object
 						CalculatePath(hit);
 					
-					} else {
-						//Debug.Log ("Player is between, or nearer");	
-						//_stopMoving = false;
 					}
 				});
 			}
 			
-			// We need to also cast a ray straight forward to check if there is an obstacle on the alternate route we take
-			// if we need to circumvent an obstacle in order to get to the player
-			
-			
-			/*
-			// more raycasts   
-			Vector3 leftRay =  _myTransform.position + new Vector3(-1.5f, 1.0f, 0.0f);
-			Vector3 rightRay = _myTransform.position + new Vector3(1.5f, 1.0f, 0.0f);
-			
-			// check for leftRay raycast
-			if (Physics.Raycast(leftRay, _myTransform.forward , out hit, _rayCastDistance)) {
-			   if (hit.transform != this.transform) {
-			    	Debug.DrawLine (leftRay, hit.point, Color.black);
-			 		
-					if(Vector3.Distance(hit.transform.position, _myTransform.position) < Vector3.Distance(hit.transform.position, target.position))
-				     	dir += hit.normal * 20; // 20 is force to repel by
-			   }
-			}
-			 
-			// check for rightRay raycast
-			if (Physics.Raycast(rightRay, transform.forward, out hit, _rayCastDistance)) {
-			   if (hit.transform != this.transform) {
-			     	Debug.DrawLine (rightRay, hit.point, Color.green);
-			 
-					if(Vector3.Distance(hit.transform.position, _myTransform.position) < Vector3.Distance(hit.transform.position, target.position))
-						dir += hit.normal * 20; // 20 is force to repel by
-			   }
-			}
-			 */
-			
-			// --
-			
+			// Move the enemy object			
 			if(!_stopMoving && _hasAggro) {
-				// Movement
-				
 				// rotation
 				var rot = Quaternion.LookRotation (dir);
-				 
-				//print ("rot : " + rot);
+			
 				transform.rotation = Quaternion.Slerp (transform.rotation, rot, Time.deltaTime);
 				 
 				//position
@@ -145,11 +105,6 @@ public class EnemyAI : MonoBehaviour {
 					_hasAggro = false;
 			}
 		}
-	
-	public void RepelBy(Vector3 force) {
-		dir += force;
-		Debug.Log ("dir: " + dir);
-	}
 	
 	/// <summary>
 	/// Anticipates a collision at a certain angle and distance when persuing a target position
@@ -205,26 +160,9 @@ public class EnemyAI : MonoBehaviour {
 			new Vector3[] {corner2, corner1}
 		};
 		
-		/*// Pair1 = corner1->corner3
-		pairs[0][0] = corner1;
-		pairs[0][1] = corner3;
-		
-		// Pair2 = corner3->corner4
-		pairs[1][0] = corner3;
-		pairs[1][1] = corner4;
-		
-		// Pair3 = corner4->corner2
-		pairs[2][0] = corner4;
-		pairs[2][1] = corner2;
-		
-		// Pair4 = corner2->corner1
-		pairs[3][0] = corner2;
-		pairs[3][1] = corner1; */
-		
 		int[] distances = new int[4];
 		
 		for(int i=0; i<pairs.Length; i++) {
-			//distances[i] = (int) (Vector3.Distance(_myTransform.position, corners[i]) + Vector3.Distance (corners[i], target.position));
 			Vector3 point1;
 			Vector3 point2;
 			
